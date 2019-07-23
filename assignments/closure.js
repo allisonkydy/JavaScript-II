11274f1778d091bd06d0aae3517ae37ede9ee625
allisonkydy <allisnelly@gmail.com>
2019-07-23 16:20:20 -0500

    completed closure stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -22,14 +22,41 @@ buzzBee();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  function increment() {
+    console.log(++count); 
+  }
+  return increment;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage: 
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+
+  function increment() {
+    console.log(++count);
+  }
+
+  function decrement() {
+    console.log(--count);
+  }
+
+  return {
+    increment: increment,
+    decrement: decrement
+  };
 };
+
+const doubleCounter = counterFactory();
+doubleCounter.increment();
+doubleCounter.increment();
+doubleCounter.decrement();
+doubleCounter.decrement();
